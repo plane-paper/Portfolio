632f02377072de2f74b158a58ad83d4436c36d9f
PlanePaper <suruiquan10@163.com>
2025-10-20 23:18:32 -0400

    chore: replaced selfie and added caption, adjusted image size

--- src/App.js
+++ src/App.js
@@ -479,7 +479,17 @@ const Portfolio = () => {
               <p>I speak English, Mandarin, and French fluently, and I have a multicultural background, being raised in two countries in my childhood. I am also fluent with most developer tools, most major languages, many modules, and many frameworks.</p>
               <p>I also love to play chess, enjoy various video games, write, or read a nice book.</p>
               <p>For professional contact, please reach me at my university email: <a href="mailto:r38su@uwaterloo.ca" className="text-blue-300 hover:text-blue-200 underline">r38su@uwaterloo.ca</a>, or my personal email: <a href="mailto:suruiquan10@163.com" className="text-blue-300 hover:text-blue-200 underline">suruiquan10@163.com</a>.</p>
-              <img src="images/pic01.jpg" alt="A photo of me" className="w-full rounded mt-6" />
+
+              <figure className="mt-6 flex flex-col items-center">
+          <img
+            src="images/pic01.jpg"
+            alt="A photo of me"
+            className="w-[90%] md:w-[400px] h-auto rounded"
+          />
+          <figcaption className="text-sm text-white/60 mt-2 italic">
+            the only time that I didn't cut my own hair and went to the barber in my whole life
+          </figcaption>
+              </figure>
             </div>
           </div>
         );
